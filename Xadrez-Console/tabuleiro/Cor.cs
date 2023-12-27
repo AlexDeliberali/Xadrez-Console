b672eabb2d16c545538b7185df841df0a4e76031
alexsander.santos <alexsander.santos@Not_GAtec>
2023-12-27 12:28:30 -0300

    Adição da classe Torre e Rei, e adição de Exceções quando ocorrer erros

---
+++ Xadrez-Console/tabuleiro/Cor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+        Azul,
+        Vermelho,
+        Amarela,
+        Verde,
+        Laranja,
+        Rosa
+    }
+}
---
+++ Xadrez-Console/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace tabuleiro
+{
+    internal class TabuleiroException : Exception
+    {
+        //Criando uma exceção personalizada
+        public TabuleiroException(string msg) : base(msg) { }
+    }
+}
