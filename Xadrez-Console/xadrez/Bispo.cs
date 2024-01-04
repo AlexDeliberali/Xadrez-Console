392869d5462b376835ef7e95bee626af59132f2a
alexsander.santos <alexsander.santos@Not_GAtec>
2024-01-04 11:35:30 -0300

    Adição das Classes Bispo,Cavalo, Dama, Peao e ajuste na instanciação do tabuleiro com todas as peças

---
+++ Xadrez-Console/xadrez/Bispo.cs
@@ -0,0 +1,80 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Bispo : Peca
+    {
+        public Bispo(Tabuleiro tab, Cor cor) : base(tab, cor) { }
+
+        public override string ToString()
+        {
+            return "B";
+        }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != cor;
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //Verificando noroeste se está livre ou com outra peça
+            pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                //Validando se há outra peça na posição acima
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.linha - 1, pos.coluna - 1);
+            }
+
+            //Verificando nordeste se está livre ou com outra peça
+            pos.definirValores(posicao.linha - 1, posicao.coluna + 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                //Validando se há outra peça na posição acima
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.linha - 1, pos.coluna + 1);
+            }
+
+            //Verificando sudeste se está livre ou com outra peça
+            pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                //Validando se há outra peça na posição acima
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.linha + 1, pos.coluna + 1);
+            }
+
+            //Verificando sudoeste se está livre ou com outra peça
+            pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                //Validando se há outra peça na posição acima
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.definirValores(pos.linha + 1, pos.coluna - 1);
+            }
+            return mat;
+        }
+    }
+}
